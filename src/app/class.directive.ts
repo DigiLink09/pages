2e11066afb623958eeb532af1bd774ac921bc602
Thulibathi <nokuthulamargaret@gmail.com>
2023-05-17 09:24:22 +0200

    Section 6 complete

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -6,8 +6,16 @@ import { Directive, ElementRef,Input } from '@angular/core';
 export class ClassDirective {
   constructor(private element : ElementRef) {}
 
-  @Input('appClass') set backgroundColor(color:string)
+  @Input('appClass') set classNames(classObj: any)
     {
-      this.element.nativeElement.style.backgroundColor = color;
+      for(let key in classObj)
+      {
+        if(classObj[key])
+        {
+          this.element.nativeElement.classList.add(key);
+        }else{
+          this.element.nativeElement.classList.remove(key);
+        }
+      }
     }
 }
